6c82fc410bdd11ec572516739550a515be19716a
Hazem Krimi <krimihazem1@gmail.com>
2023-12-08 23:12:57 +0100

    Improve formatting

--- src/main/kotlin/DayOne.kt
+++ src/main/kotlin/DayOne.kt
@@ -6,11 +6,13 @@ import kotlin.io.path.absolutePathString
 
 fun dayOne() {
     val calibrations: Int = File(Path("src/main/resources/day-one.txt").absolutePathString()).useLines { lines ->
-        lines.map {
-            val digits = it.filter { ch -> ch.isDigit() }
+        lines
+            .map {
+                val digits = it.filter { ch -> ch.isDigit() }
 
-            "${digits[0]}${digits[digits.length - 1]}".toInt()
-        }.sum()
+                "${digits[0]}${digits[digits.length - 1]}".toInt()
+            }
+            .sum()
     }
 
     println(calibrations)
