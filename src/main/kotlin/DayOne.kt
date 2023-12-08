a550552c64870ce53170371678411d7a92ffd037
Hazem Krimi <krimihazem1@gmail.com>
2023-12-08 23:10:33 +0100

    Day one part one

---
+++ src/main/kotlin/DayOne.kt
@@ -0,0 +1,17 @@
+package adventOfCode
+
+import java.io.File
+import kotlin.io.path.Path
+import kotlin.io.path.absolutePathString
+
+fun dayOne() {
+    val calibrations: Int = File(Path("src/main/resources/day-one.txt").absolutePathString()).useLines { lines ->
+        lines.map {
+            val digits = it.filter { ch -> ch.isDigit() }
+
+            "${digits[0]}${digits[digits.length - 1]}".toInt()
+        }.sum()
+    }
+
+    println(calibrations)
+}
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,12 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+package adventOfCode
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+fun main(args: Array<String>) {
+    try {
+        when (args[0]) {
+            "1" -> dayOne()
+            else -> dayOne()
+        }
+    } catch (exception: ArrayIndexOutOfBoundsException) {
+        dayOne()
+    }
 }
